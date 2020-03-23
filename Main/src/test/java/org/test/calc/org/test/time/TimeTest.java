99edd82b9af5e00d3a8099dd7d9c75936b744c53
Galkin Anton <galkin.a.bo@sberbank.ru>
2020-03-23 17:55:18 +0300

    update

---
+++ Main/src/test/java/org/test/calc/org/test/time/TimeTest.java
@@ -0,0 +1,42 @@
+/*
+ * Copyright (c) 2008-2020
+ * Sberbank
+ * All rights reserved.
+ *
+ * This product and related documentation are protected by copyright and
+ * distributed under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or related documentation may be
+ * reproduced in any form by any means without prior written authorization of
+ * Sberbank and its licensors, if any.
+ *
+ * $
+ */
+package org.test.calc.org.test.time;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.Instant;
+import java.time.Month;
+import java.time.ZonedDateTime;
+import java.util.Arrays;
+
+/**
+ * Project: TestTest
+ * Author: Galkin A.B.
+ * Date: 23.03.2020
+ * Time: 17:42
+ * Descriptions
+ */
+
+public class TimeTest {
+    private void print(Object... args) {
+        Arrays.stream(args).map(Object::toString).forEach(System.out::println);
+    }
+
+    @Test
+    public void instantTime() {
+        print("Time now ", Instant.now());
+        print("Zoned now ", ZonedDateTime.now());
+        print(Month.APRIL, Month.of(12));
+    }
+}
