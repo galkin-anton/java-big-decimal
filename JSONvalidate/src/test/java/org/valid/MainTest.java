d2cb760caa8fb85fd9ab2d5ad4de66a187feb847
Galkin Anton <galkin88@gmail.com>
2020-07-16 12:30:59 +0300

    updated

---
+++ JSONvalidate/src/test/java/org/valid/MainTest.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2008-2020
+ * Sberbank
+ * All rights reserved.
+ *
+ * This product and related documentation are protected by copyright and
+ * distributed under licenses restricting its use, copying, distribution, and
+ * decompilation. No part of this product or related documentation may be
+ * reproduced in any form by any means without prior written authorization of
+ * Sberbank and its licensors, if any.
+ *
+ * $
+ */
+package org.valid;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Project: java-big-decimal
+ *
+ * <p>Author: Galkin
+ *
+ * <p>Date: 16.07.2020
+ *
+ * <p>Time: 12:29 Descriptions
+ */
+public class MainTest {
+  @Test
+  public void test1() {
+    System.out.println("Hi");
+  }
+}
